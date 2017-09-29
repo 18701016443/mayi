74c44015854369f99dde408c4e5fab6c0896f663
18701016443 <757200708@qq.com>
2017-09-29 18:10:04 +0800

    PC二维码

--- mayi/test_case/page_obj/mayi_QR_page.py
+++ mayi/test_case/page_obj/mayi_QR_page.py
@@ -20,12 +20,63 @@ class MaYiQRPage(Pyse):
         QR_url = self.get_attribute("xpath=>/html/body/div[1]/div/ul/li[3]/div/div/img","src")
         return QR_url
 
-    #下载页——下载按钮
-    def down_btn(self):
-        self.click("xpath=>/html/body/section/div[1]/div/a")
+    #首页右侧保洁合作——APP首单立减5元——二维码
+    def right_QR(self):
+        self.move_to_element("xpath=>//*[@id='app-click']/a/b")
+        QR_url = self.get_attribute("xpath=>//*[@id='top_div']/img","src")
+        return QR_url
+
 
     #谷歌下载页面文案打印
     def text(self):
         text = self.get_text("xpath=>/html/body")
         return text
 
+    #首页底部二维码
+    def bottom_QR(self):
+        QR_url = self.get_attribute("css=>.mt10,.app_download_qr","src")
+        return QR_url
+
+    #首页底部Android下载
+    def Androiddown_btn(self):
+        self.click("css=>.app,.mt10 t-center")
+
+    #下载蚂蚁短租客户端手机注册立送5元——二维码
+    def bottom_left_QR(self):
+        QR_url = self.get_attribute("xpath=>//*[@id='floatingLayer']/div[1]/div[2]/div[1]/img","src")
+        return QR_url
+
+    #下载蚂蚁短租客户端手机注册立送5元——下载按钮
+    def bottom_left_andriodbtn(self):
+        self.click("xpath=>//*[@id='floatingLayer']/div[1]/div[2]/div[2]/a[2]")
+
+    #APP50下载页面的Android下载按钮
+    def APP50_andriodbtn(self):
+        self.click("id=>Android")
+
+    #房东banner图上的二维码
+    def order_banner_QR(self):
+        QR_url = self.get_attribute("xpath=>//*[@id='box']/div[1]/ul/li[1]/img","src")
+        return QR_url
+
+    #下载页——拨打电话——二维码
+    def call_QR(self):
+        self.move_to_element("class=>call-phone")
+        QR_url = self.get_attribute("class=>chat_ma","src")
+        return QR_url
+
+    #房客订单管理右侧
+    def tenant_order_QR(self):
+        QR_url = self.get_attribute("xpath=>//*[@id='wx']/a/img","src")
+        return QR_url
+
+    #房东招募
+    def landlord_zhaomu_QR(self):
+        QR_url = self.get_attribute("xpath=>/html/body/div[13]/div[2]/div[2]/div[2]/div[3]/div[3]/img","src")
+        return QR_url
+    # def close_(self):
+    #     self.click("xpath=>//*[@id='floatingLayer']/div[1]/div[2]/div[5]/img")
+
+    # 下载页——下载按钮
+    def down_btn(self):
+        self.click("xpath=>/html/body/section/div[1]/div/a")
--- mayi/test_case/test_mayiQR.py
+++ mayi/test_case/test_mayiQR.py
@@ -7,7 +7,7 @@
 @software: PyCharm
 @time: 2017/9/28 17:31
 """
-from mayi.test_case.page_obj import mayi_QR_page
+from mayi.test_case.page_obj import mayi_QR_page,login_page,landlord_nav_page,tenant_nav_page
 from mayi.models import jiexi_QR,myunit
 from selenium import webdriver
 from time import sleep
@@ -16,22 +16,23 @@ import unittest
 class TestQR(myunit.MyTest):
     '''二维码解析'''
 
-    def test_nav_QR(self):
-        # driver = webdriver.Chrome()
-        # driver.get("http://pre.mayi.com")
-        po = mayi_QR_page.MaYiQRPage(self.driver)
-        po.open()
+    def login_online(self):
+        '''线上登录'''
+        login_page.LoginPage(self.driver).loginshow()
+        login_page.LoginPage(self.driver).changeloginbyup()
+        login_page.LoginPage(self.driver).username("18701016443")
+        login_page.LoginPage(self.driver).password("18701016443")
+        login_page.LoginPage(self.driver).imagecode1()
+        login_page.LoginPage(self.driver).loginbyupdo()
 
-        url = jiexi_QR.decode_qr(po.nav_qr())
-        sleep(3)
-        # driver.get(url)
-        po.opentest(url)
-        sleep(2)
-        po.down_btn()
-        sleep(2)
+    def public_google_downpage(self,url):
+        # 进入谷歌下载页面
         self.driver.get("chrome://downloads/")
         sleep(2)
-        text = po.text()
+
+        # 打印body的所有内容，然后去截断获取自己想要的
+        text = mayi_QR_page.MaYiQRPage(self.driver).text()
+
         sleep(2)
         text = text.split("\n")
         APP_name = text[5]
@@ -42,9 +43,244 @@ class TestQR(myunit.MyTest):
         APP_size = size[1]
         sleep(2)
         print("")
-        print("APP包名：" +APP_name)
-        print("APP下载地址："+APP_url)
-        print("APP大小："+APP_size)
+        print("二维码解析结果：" + url)
+        print("APP包名：" + APP_name)
+        print("APP下载地址：" + APP_url)
+        print("APP大小：" + APP_size)
+
+
+    def test_nav_QR(self):
+        '''首页导航——APP首单立减5元——二维码'''
+        po = mayi_QR_page.MaYiQRPage(self.driver)
+
+
+        #获取最新的二维码url，并传到解析方法中。也可直接把url直接传，考虑二维码url可能会改变，因此每次都获取最新的。
+        url = jiexi_QR.decode_qr("http://staticnew.mayi.com/resourcesWeb/v201510/images/commom/app.jpg")
+        sleep(3)
+        #打开解析后的下载地址
+        po.opentest(url)
+        sleep(2)
+
+        #下载按钮
+        po.down_btn()
+        sleep(2)
+
+        #调用公共函数，打印想获取的数据
+        self.public_google_downpage(url)
+
+    def test_bottom_QR(self):
+        '''首页底部二维码'''
+        po = mayi_QR_page.MaYiQRPage(self.driver)
+
+        # 获取最新的二维码url，并传到解析方法中。也可直接把url直接传，考虑二维码url可能会改变，因此每次都获取最新的。
+        url = jiexi_QR.decode_qr("http://staticnew.mayi.com/resourcesWeb/v201510/images/commom/app.jpg")
+        sleep(3)
+        # 打开解析后的下载地址
+        po.opentest(url)
+        sleep(2)
+
+        # 下载按钮
+        po.down_btn()
+        sleep(2)
+
+        # 调用公共函数，打印想获取的数据
+        self.public_google_downpage(url)
+
+
+    def test_Androiddown_btn(self):
+        '''首页底部Android下载按钮'''
+        po = mayi_QR_page.MaYiQRPage(self.driver)
+        po.opentest("http://www.mayi.com")
+        po.js("var j = document.body.scrollTop = 4935")
+
+        sleep(3)
+
+        po.Androiddown_btn()
+        url = "直接下载apk"
+        sleep(3)
+
+        # 调用公共函数，打印想获取的数据
+        self.public_google_downpage(url)
+
+
+    def test_bottom_left_QR(self):
+        '''下载蚂蚁短租客户端手机注册立送5元——二维码'''
+        po = mayi_QR_page.MaYiQRPage(self.driver)
+
+        url = jiexi_QR.decode_qr("http://staticnew.mayi.com/resourcesWeb/v201510/images/commom/app.jpg")
+        sleep(3)
+        # 打开解析后的下载地址
+        po.opentest(url)
+        sleep(2)
+
+        # 下载按钮
+        po.down_btn()
+        sleep(2)
+
+        # 调用公共函数，打印想获取的数据
+        self.public_google_downpage(url)
+
+    def test_bottom_left_Andriodbtn(self):
+        '''下载蚂蚁短租客户端手机注册立送5元——Android下载按钮'''
+        po = mayi_QR_page.MaYiQRPage(self.driver)
+        po.opentest("http://www.mayi.com")
+        sleep(3)
+        po.js("var j = document.body.scrollTop = 1000")
+        po.bottom_left_andriodbtn()
+        url = "直接下载APK"
+        sleep(3)
+
+        # 调用公共函数，打印想获取的数据
+        self.public_google_downpage(url)
+
+    def test_APP50_QR(self):
+        '''APP50下载页面的二维码'''
+        po = mayi_QR_page.MaYiQRPage(self.driver)
+
+        url = jiexi_QR.decode_qr("http://staticnew.mayi.com/resourcesWeb/topic/app50/images/fkerwei.png")
+        sleep(3)
+        # 打开解析后的下载地址
+        po.opentest(url)
+        sleep(2)
+
+        # 下载按钮
+        po.down_btn()
+        sleep(2)
+
+        # 调用公共函数，打印想获取的数据
+        self.public_google_downpage(url)
+
+    def test_APP50_andriodbtn(self):
+        '''APP50下载页面的Android下载按钮'''
+        po = mayi_QR_page.MaYiQRPage(self.driver)
+        po.opentest("http://www.mayi.com/activity/app50")
+        sleep(3)
+        po.APP50_andriodbtn()
+        url = "直接下载APK"
+        sleep(3)
+
+        # 调用公共函数，打印想获取的数据
+        self.public_google_downpage(url)
+
+    def test_order_banner_QR(self):
+        '''房东我的订单banner图第一张——二维码'''
+        po = mayi_QR_page.MaYiQRPage(self.driver)
+        po.opentest("http://www.mayi.com")
+        #登录
+        self.login_online()
+        sleep(5)
+        landlord_nav_page.LandlordNavPage(self.driver).Iamlandlord()
+        sleep(3)
+        landlord_nav_page.LandlordNavPage(self.driver).close_weiChat()
+
+
+        url = jiexi_QR.decode_qr(po.order_banner_QR())
+        sleep(3)
+        # 打开解析后的下载地址
+        po.opentest(url)
+        sleep(2)
+
+        # 下载按钮
+        po.down_btn()
+        sleep(2)
+
+        # 调用公共函数，打印想获取的数据
+        self.public_google_downpage(url)
+
+    def test_index_rigit_QR(self):
+        '''首页右侧保洁合作下面——APP首单立减5元——二维码'''
+        po = mayi_QR_page.MaYiQRPage(self.driver)
+        po.opentest("http://www.mayi.com")
+        po.js("var j = document.body.scrollTop = 1000")
+
+
+        url = jiexi_QR.decode_qr(po.right_QR())
+        sleep(3)
+        # 打开解析后的下载地址
+        po.opentest(url)
+        sleep(2)
+
+        # 下载按钮
+        po.down_btn()
+        sleep(2)
+
+        # 调用公共函数，打印想获取的数据
+        self.public_google_downpage(url)
+
+    def test_order_call_QR(self):
+        '''下单页——拨打电话——二维码'''
+        po = mayi_QR_page.MaYiQRPage(self.driver)
+        po.opentest("http://www.mayi.com/room/851272901")
+
+
+        url = jiexi_QR.decode_qr(po.call_QR())
+        sleep(3)
+        # 打开解析后的下载地址
+        po.opentest(url)
+        sleep(2)
+
+        # 下载按钮
+        po.down_btn()
+        sleep(2)
+
+        # 调用公共函数，打印想获取的数据
+        self.public_google_downpage(url)
+
+    def test_tenant_order_QR(self):
+        '''房客我的订单右侧二维码'''
+        po = mayi_QR_page.MaYiQRPage(self.driver)
+        po.opentest("http://www.mayi.com/")
+        self.login_online()
+        sleep(3)
+        tenant_nav_page.TenantNavPage(self.driver).Iamtenant()
+
+
+        url = jiexi_QR.decode_qr(po.tenant_order_QR())
+        sleep(3)
+        # 打开解析后的下载地址
+        po.opentest(url)
+        sleep(2)
+
+        # 下载按钮
+        po.down_btn()
+        sleep(2)
+
+        # 调用公共函数，打印想获取的数据
+        self.public_google_downpage(url)
+
+    def test_msg_QR(self):
+        '''IM聊天的二维码'''
+        po = mayi_QR_page.MaYiQRPage(self.driver)
+
+        url = jiexi_QR.decode_qr("http://staticnew.mayi.com/resourcesWeb/v201510/images/app_down_im.png")
+        sleep(3)
+        # 打开解析后的下载地址
+        po.opentest(url)
+        sleep(2)
+
+        # # 下载按钮
+        # po.down_btn()
+        # sleep(2)
+
+        # 调用公共函数，打印想获取的数据
+        self.public_google_downpage(url)
+
+    def test_landlord_zhaomu_QR(self):
+        '''PC房东招募的二维码'''
+        po = mayi_QR_page.MaYiQRPage(self.driver)
+
+        url = jiexi_QR.decode_qr("http://staticnew.mayi.com/resourcesWeb/landlordRecruit/images/code.png")
+        sleep(3)
+        # 打开解析后的下载地址
+        po.opentest(url)
+        sleep(2)
+
+        # # 下载按钮
+        po.down_btn()
+        sleep(2)
+
+        # 调用公共函数，打印想获取的数据
+        self.public_google_downpage(url)
 
 if __name__ == "__main__":
     unittest.main()
